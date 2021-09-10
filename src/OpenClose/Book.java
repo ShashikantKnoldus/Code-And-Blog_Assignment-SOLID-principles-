5da54c29d5c56c75795352bc4bbf6ee273c498cb
ShashikantKnoldus <shashikant.tyagi@knoldus.com>
2021-09-10 11:28:31 +0530

    first commit

--- src/OpenClose/Book.java
+++ src/OpenClose/Book.java
@@ -9,22 +9,6 @@ public class Book {
         name="Bhagavad Gita";
         author="Vyasa";
     }
-//    void showDetails(){
-//        System.out.println("The ID of the Book is "+id);
-//        System.out.println("The Name of the Book is "+name);
-//        System.out.println("The Author of the Book is "+author);
-//    }
 
-//    void enterDetails(){
-//        Scanner sc= new Scanner(System.in);
-//        System.out.print("Enter the ID of the new book - ");
-//        id = sc.nextInt();
-//        System.out.print("Enter the name of the new book - ");
-//        name = sc.next();
-//        System.out.print("Enter the author of the new book - ");
-//        author= sc.next();
-
-
-    //   }
 }
 
--- src/OpenClose/User.java
+++ src/OpenClose/User.java
@@ -11,25 +11,4 @@ public class User {
         address="Muzaffarnagar";
         contactNumber= 123456789;
     }
-
-//    void showDetails(){
-//        System.out.println("The userID  is "+userId);
-//        System.out.println("The Name of the "+userId+ "is "+name);
-//        System.out.println("The " +name+ "'s address is "+address);
-//        System.out.println("The " +name+ "'s contact number is "+contactNumber);
-//
-//    }
-
-//    void enterDetails(){
-//        Scanner sc= new Scanner(System.in);
-//        System.out.print("Enter the ID of the new user");
-//        userId = sc.nextInt();
-//        System.out.print("Enter the name of the new user");
-//        name = sc.next();
-//        System.out.print("Enter the address of the new user");
-//        address= sc.next();
-//        System.out.print("Enter the contact number of the new user");
-//        contactNumber= sc.nextInt();
-    //}
-
 }
